a55a11f2d11ec271f208f36381a86e43fefbb8f3
Lopll <maks.maksim03@mail.ru>
2025-03-17 09:22:07 +0300

    ExitButton+Fix

--- LuaModules/Button.lua
+++ LuaModules/Button.lua
@@ -5,7 +5,7 @@ local BUTTON_FUNC_BLOCK_TIME = 0.2 -- after click button will be disabled for th
 local buttonInProgress = false -- preventing missclicks
 
 function M.new(buttonNode, inputZoneNode, defaultColor, buttonFunc, funcArgs, hoverSound, clickSound, 
-    parentNode)
+    parentNode, scale)
 local button =
 {
     buttonNode = buttonNode, -- button node from gui
@@ -16,16 +16,17 @@ local button =
     hoverSound = hoverSound, -- sound that plays on hover
     clickSound = clickSound, -- sound that plays on click
     wasClicked = false, -- value to disable button actions for some time after click 
-    parentNode = parentNode -- node that is like a parent to the button. If parent is invisible
+    parentNode = parentNode, -- node that is like a parent to the button. If parent is invisible
                             -- button won't work
+    scale = scale
 }
 return button
 end
 
 function M.unhover(button)
-    if gui.get_scale(button.buttonNode) == HOVER_SCALE then -- was button already unhovered?
+    if gui.get_scale(button.buttonNode) == button.scale * 1.1 then -- was button already unhovered?
         -- over hover
-        gui.set_scale(button.buttonNode, vmath.vector3(1, 1, 1))
+        gui.set_scale(button.buttonNode, button.scale)
         gui.set_color(button.buttonNode, button.defaultColor)
     end
 end
@@ -33,19 +34,19 @@ end
 function M.on_input(self, action_id, action, button)
     local parent = button.parentNode ~= nil and button.parentNode or gui.get_parent(button.buttonNode)
     
-    if defos.is_mouse_in_view() and gui.get_alpha(parent) == 1 and gui.get_visible(button.buttonNode) then -- is cursor inside visible button?
+    if defos.is_mouse_in_view() and gui.is_enabled(parent) and gui.get_visible(button.buttonNode) then -- is cursor inside visible button?
         if (button.inputZone == nil and gui.pick_node(button.buttonNode, action.x, action.y) ) 
             or (button.inputZone ~= nil and gui.pick_node(button.inputZone, action.x, action.y)) then -- is cursor inside the input zone?
             if not button.wasClicked or action.released then -- was button clicked? if yes, it is blocked for some time 
                 -- hover sound
                 if button.hoverSound ~= nil 
-                    and gui.get_scale(button.buttonNode) ~= HOVER_SCALE then -- is hover sound set and wasn't played?
+                    and gui.get_scale(button.buttonNode) ~= button.scale * 1.1 then -- is hover sound set and wasn't played?
                     -- play hover sound
                     msg.post(SCENE_MANAGER, "PlaySound", {sound = button.hoverSound})
                 end
                 
                 -- visual hover
-                gui.set_scale(button.buttonNode, HOVER_SCALE)
+                gui.set_scale(button.buttonNode, button.scale * 1.1)
                 gui.set_color(button.buttonNode, vmath.vector4(1, 1, 1, 1))
                 
                 if action_id == hash("Touch") and action.pressed and not buttonInProgress then -- was left mouse pressed?
@@ -55,7 +56,7 @@ function M.on_input(self, action_id, action, button)
                     button.buttonFunc(button.funcArgs)
                     
                     -- over hover while click
-                    gui.set_scale(button.buttonNode, vmath.vector3(1, 1, 1))
+                    gui.set_scale(button.buttonNode, button.scale)
                     gui.set_color(button.buttonNode, button.defaultColor)
                     
                     -- disable button for some time
